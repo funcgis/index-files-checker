1c1df8ec7573c4e9128cb95bb239a5ff7a74b1b6
chale <funcgis@gmail.com>
2017-12-27 20:32:59 +0800

    添加主脚本及测试文件

---
+++ check.py
@@ -0,0 +1,131 @@
+# __*__ coding:utf-8 __*__
+__author__ = 'Chale'
+__date__ = '2017/12/27 9:27'
+__describe__ = u'检查索引文件与实际数据文件的一致性，需满足索引文件中的文件名与实际文件文件名一致'
+__email__ = 'funcgis@163.com'
+
+import sys
+import getopt
+import os
+
+
+def list_index(index):
+    print u"正在扫描索引文件..."
+    f = open(index)  # 返回一个文件对象
+    line = f.readline()  # 调用文件的 readline()方法
+    lines = []
+    if line is not None and line != "":
+        lines.append(line.replace("\n", ""))
+    while line:
+        line = f.readline()
+        if line is not None and line != "":
+            lines.append(line.replace("\n", ""))
+    return lines
+
+
+def list_folder(folder):
+    print u"正在扫描目录..."
+    listfiles = []
+    for root, dirs, files in os.walk(folder):
+        listfiles = files
+        # print(root)  # 当前目录路径
+        # print(dirs)  # 当前路径下所有子目录
+        # print(files)  # 当前路径下所有非目录子文件
+    return listfiles
+
+
+def save_list(listData, savePath):
+    print u'正在输出到：', savePath
+    file = open(savePath, 'w')
+    for i in listData:
+        file.write(str(i) + '\n')
+    file.close()
+
+
+def check(index, folder, out, ifOut, ifRight):
+    if os.path.exists(index):
+        lines = list_index(index)
+    else:
+        print u'输入索引文件不存在！'
+    if os.path.exists(folder):
+        files = list_folder(folder)
+    else:
+        print u'输入文件夹不存在！'
+
+    listlines = []
+    listfiles = []
+
+    if lines is not None and len(lines) >= 0:
+        if files is not None and len(files) >= 0:
+            listlines = list(set(lines).difference(set(files)))
+            print u'未含索引文件', listlines
+            if ifRight:
+                listfiles = list(set(files).difference(set(lines)))
+                print u'未含目录文件', listfiles
+            if ifOut:
+                if out is not None and out != "":
+                    if os.path.exists(out):
+                        save_list(listlines, out + '\\notInIndex.txt')
+                    if len(listfiles) > 0:
+                        save_list(listfiles, out + '\\notInFolder.txt')
+                else:
+                    save_list(listlines, os.getcwd() + '\\notInIndex.txt')
+                    if len(listfiles) > 0:
+                        save_list(listfiles, os.getcwd() + '\\notInFolder.txt')
+        else:
+            print u'文件夹为空！'
+    else:
+        print u'索引文件为空！'
+
+
+def get_help():
+    print u"作者：", __author__, '\n', u"编写日期：", __date__, '\n', \
+        u"描述：", __describe__, '\n', u"联系方式：", __email__
+    print '\n', u"使用方式：", '\n', u"-i         输入索引文件", '\n', u"-f         输入实际文件文件夹", '\n'\
+        u"-k         是否输出结果文件（可选）", \
+        '\n', u"-o         与 -k 一起使用，指定输出文件路径，默认为程序执行当前路径（可选）",\
+        '\n', u"-r         是否反向对比（即文件夹中存在，索引文件中不存在）（可选）", '\n', u"-h         查看帮助"
+    print '\n', u"使用示例：", '\n', u"python ./check.py -i index.txt -f E:\\test -o E:\\test"
+
+
+def get_error():
+    print u"请输入 -i, -f 两个必须参数，详细请输入 -h 查看帮助"
+
+
+def main(argv):
+    indexfile = u""
+    foldername = u""
+    outfile = u""
+    ifOut = False
+    ifRight = False
+
+    try:
+        opts, args = getopt.getopt(argv, "hkri:f:o:", ["indexfile=", "foldername=", "outfile="])
+    except getopt.GetoptError:
+        get_error()
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == "-h":
+            get_help()
+            sys.exit()
+        elif opt in ("-i", "--indexfile"):
+            indexfile = arg
+        elif opt in ("-f", "--foldername"):
+            foldername = arg
+        elif opt in ("-o", "--outfile"):
+            outfile = arg
+        elif opt in ("-r", "--ifRight"):
+            ifRight = True
+        elif opt in ("-k", "--ifOut"):
+            ifOut = True
+
+    if indexfile != "" and foldername != "":
+        check(indexfile, foldername, outfile, ifOut, ifRight)
+    else:
+        get_error()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
